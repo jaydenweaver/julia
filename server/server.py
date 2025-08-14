ad4b2476ad77ecfd71a4d15bc45db12b038a48b3
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-14 12:57:58 +1000

    map time to julia constants

--- server/server.py
+++ server/server.py
@@ -2,19 +2,46 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 import uuid
 import os
-os.makedirs("uploads", exist_ok=True)
+import numpy as np
+import hashlib
+import requests
+
+os.makedirs("images", exist_ok=True)
+
+api_url = 'http://worldtimeapi.org/api/timezone'
+
+
+def get_time(country, city):
+    url = f"{api_url}/{country}/{city}"
+    try:
+        res = requests.get(url)
+        res.raise_for_status()
+        data = res.json()
+        return data['unixtime'], data['day_of_year']
+    except requests.RequestException as e:
+        print(f"api request failed: {e}")
+    return None
+
+
+def hash_tuple(vals):
+    seed = f"{vals[0]}_{vals[1]}"
+    hash_bytes = hashlib.sha256(seed.encode('utf-8')).digest()
+    hash_a = int.from_bytes(hash_bytes, "big")
+
+    seed = f"{vals[0]}_{vals[1]}_second"
+    hash_bytes = hashlib.sha256(seed.encode('utf-8')).digest()
+    hash_b = int.from_bytes(hash_bytes, "big")
+
+    return (hash_a, hash_b)
+
+
+# maps hash_tuple (256bit ints) to a range between -1.5, 1.5
+def hash_to_julia_constant(val):
+    norm_a = val[0] / (2**256 - 1)
+    norm_b = val[1] / (2**256 - 1)
+    return -1.5 + norm_a * (1.5 - (-1.5)), -1.5 + norm_b * (1.5 - (-1.5))
+
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="../client/dist", html=True), name="static")
-
-
-@app.post("/upload")
-async def upload_video(file: UploadFile = File(...)):
-    if not file.content_type.startswith("video/"):
-        return {"error": "invalid file type"}
-
-    # prepend unique id to avoid filename collisions
-    file_path = f"uploads/{uuid.uuid4()}_{file.filename}"
-    with open(file_path, "wb") as f:
-        f.write(await file.read())
-    return {"filename": file.filename, "message": "upload successful"}
+app.mount("/", StaticFiles(directory="../client/dist", html=True),
+          name="static")

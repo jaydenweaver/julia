bc0905f31a5465e9fc3547f2de41e2e5a30e97f3
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-18 20:24:16 +1000

    cache images

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ import uuid
 import os
 import shutil
 from juliaset import create_julia_image
+from datetime import datetime
 
 save_dir = "images"
 
@@ -11,6 +12,7 @@ save_dir = "images"
 shutil.rmtree(save_dir)
 os.makedirs(save_dir, exist_ok=True)
 
+julia_cache = {}
 
 users = {
     'user': {
@@ -29,11 +31,25 @@ app = FastAPI()
 
 @app.get("/time/{country}/{city}")
 async def get_julia_image_time(country: str, city: str):
+    # get cache key
+    time_key = datetime.utcnow().strftime("%Y-%m-%d-%H-%M")
+    key = (country.lower(), city.lower(), time_key)
+
+    # check if we already have the request in cache
+    if key in julia_cache:
+        file_path = julia_cache[key]
+        if os.path.exists(file_path):
+            return FileResponse(file_path, media_type="image/png")
+        else:
+            del julia_cache[key]
+
     img = await create_julia_image(country=country, city=city)
     if img is None:
-        return {'works': 'None'}
-    unique_id = str(uuid.uuid4())
-    file_name = f"{unique_id}_{country}_{city}.png"
+        return {'image creation failed'}
+
+    file_name = f"{key}.png"
     file_path = os.path.join(save_dir, file_name)
     img.save(file_path)
+    julia_cache[key] = file_path  # store in cache
+
     return FileResponse(file_path, media_type="image/png")
